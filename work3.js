2683d2af79aea510df1813f35f0e30f6cd83f58b
ilyayak <54434312+ilyayak@users.noreply.github.com>
2020-11-12 19:26:19 +0700

    Add files via upload

---
+++ work3.js
@@ -0,0 +1,11 @@
+   
+      const path = "/users/download/index.html"
+      var k='html';
+      if (path.slice(-4)==k){
+       var isHtml=true;
+      }
+      else{
+        var isHtml=false;
+      }
+      console.log(isHtml);
+     
